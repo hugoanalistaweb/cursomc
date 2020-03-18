f8a5dc4317bd2d18721b9dd8d1b37332d852b6df
Hugo Miranda <hugo.analistaweb@gmail.com>
2020-03-18 19:07:12 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/hugo/cursomc/services/DBService.java
+++ src/main/java/com/hugo/cursomc/services/DBService.java
@@ -121,7 +121,7 @@ private ItemPedidoRepository itemPedidoRepository;
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
-		Cliente cli1 = new Cliente(null,"Maria Silva","maria@gmail.com","36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null,"Maria Silva","hugo.analistaweb@gmail.com","36378912377", TipoCliente.PESSOAFISICA);
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
 		
